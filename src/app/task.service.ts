7f1cb778c8a129d2af1fc1e0a73de13d38261a26
NetanelYaso <netanelyaso12@gmail.com>
2024-04-17 02:15:06 +0300

    deleting comments

--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -11,7 +11,6 @@ export class TaskService {
   }
 
   createList(title: string) {
-    // We want to send a web request to create a list
     return this.webReqService.post('lists', { title });
   }
 
